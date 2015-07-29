55935c3692b8dbb2611450d63d877fc1cfbb7b7d
MrBlackadder <aidanganly@gmail.com>
2015-07-29 18:43:49 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1699,6 +1699,19 @@
                     }
                 }
             },
+            
+            seduceCommand: {
+                command: 'seduce',
+                rank: 'residentdj',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.seduce);
+                    }
+                }
+            },
 
             
 
