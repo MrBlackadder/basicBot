0c49252cf00a40b6092c01efdd8c4a103f64e6cd
Aidan <aidanganly@gmail.com>
2015-07-31 12:01:19 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1702,18 +1702,7 @@
                 }
             },
             
-            seduceCommand: {
-                command: 'seduce',
-                rank: 'residentdj',
-                type: 'exact',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        API.sendChat(subChat(basicBot.chat.seduce, {name: chat.un, link: basicBot.settings.seduceLink}));
-                    }
-                }
-            },
+            
 
             
 
