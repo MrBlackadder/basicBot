c11a181fc7254d89a90dbd54e102a880ad999fc2
Aidan <aidanganly@gmail.com>
2015-07-31 12:09:21 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2470,7 +2470,7 @@
                         if (msg.length <= cmd.length + 1) return API.sendChat(subChat(basicBot.chat.currentlang, {language: basicBot.settings.language}));
                         var argument = msg.substring(cmd.length + 1);
 
-                        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
+                        $.get("https://rawgit.com/MrBlackadder/basicBot/master/lang/langIndex.json", function (json) {
                             var langIndex = json;
                             var link = langIndex[argument.toLowerCase()];
                             if (typeof link === "undefined") {
