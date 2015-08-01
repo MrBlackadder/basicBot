d6b75f4088c7a4fca591b3240803c6dec9072a41
Aidan <aidanganly@gmail.com>
2015-08-01 22:28:32 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2994,6 +2994,20 @@
                     }
                 }
             },
+            
+            dorokCommand: {
+                command: 'dorok',
+                rank: 'user',
+                type: 'startsWith',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        if (typeof basicBot.settings.rulesLink === "string")
+                            return API.sendChat(subChat({link: basicBot.settings.ruleLink}));
+                    }
+                }
+            },
 
             sessionstatsCommand: {
                 command: 'sessionstats',
