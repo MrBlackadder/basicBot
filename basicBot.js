217eb7386a9429c7eae2ac83276ee8d9db534547
Aidan <aidanganly@gmail.com>
2015-08-04 23:22:18 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -236,7 +236,7 @@
     var botCreatorIDs = ["3851534", "4105209"];
 
     var basicBot = {
-        version: "2.8.10",
+        version: "3.0",
         status: false,
         name: "basicBot",
         loggedInID: null,
