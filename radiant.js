1ada25c41d3c80643e0c298d10dcb80be1556d17
Aidan <aidanganly@gmail.com>
2016-06-05 01:45:31 +0100

    Create radiant.js

---
+++ radiant.js
@@ -0,0 +1,13 @@
+API.on(API.CHAT, listener);
+
+{
+
+    type: <string> // "message", "emote", "moderation", "system"
+
+    un: <string> // the username of the sender
+
+    uid: <int> // the user id of the sender
+
+    message: <string> // the chat message
+
+}
