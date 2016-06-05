1a772f33497b114b4b8d0371aa49bdc76b298741
Aidan <aidanganly@gmail.com>
2016-06-05 01:35:27 +0100

    Update stuck.js

--- stuck.js
+++ stuck.js
@@ -9,6 +9,7 @@ API.on(API.ADVANCE, function () {
     // skip if we're still on the same song
     if (API.getMedia().cid === current.cid) {
       API.moderateForceSkip()
+      API.sendChat('Song stuck, skipping...');
     }
   }, (current.duration + TIMEOUT) * 1000)
 })
